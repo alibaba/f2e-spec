55b6a68fe01f40a6a58df5a2efb9ed93dcd7a889
Julien <lakerswgq@gmail.com>
2020-12-15 15:34:47 +0800

    fix(eslint-plugin-ali): remove "ali/no-broad-semantic-versioning" from recommended config

--- packages/eslint-plugin-ali/src/configs/recommended.js
+++ packages/eslint-plugin-ali/src/configs/recommended.js
@@ -1,19 +1,5 @@
 module.exports = {
   plugins: ['ali'],
-  overrides: [
-    {
-      files: ['package.json'],
-      processor: 'ali/.json',
-      rules: {
-        'ali/no-broad-semantic-versioning': 'error',
-        'quotes': 'off',
-        'semi': 'off',
-        'eol-last': 'off',
-        'quote-props': 'off',
-        'comma-dangle': 'off',
-      }
-    },
-  ],
   rules: {
     'ali/no-http-url': 'warn',
     'ali/no-secret-info': 'error',
