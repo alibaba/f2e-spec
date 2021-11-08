2798b0d1f464a3bf1f499b8983ccaf95cbea351b
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2021-11-08 14:19:09 +0800

    fix: commitlint config read path

--- packages/f2elint/src/lints/markdownlint.ts
+++ packages/f2elint/src/lints/markdownlint.ts
@@ -21,9 +21,17 @@ export const getLintConfig = (opts: ScanOptions): LintOptions => {
   // 使用默认的 lint 配置
   const lintConfigFiles = glob.sync('.markdownlint(.@(yaml|yml|json))', { cwd });
   if (lintConfigFiles.length === 0) {
-    lintConfig.config = markdownLint.readConfigSync(
-      path.resolve(__dirname, '../../node_modules/markdownlint-config-ali/index.json'),
-    );
+    try {
+      // markdownlint-config-ali 与 f2elint 位于同一级 node_modules
+      lintConfig.config = markdownLint.readConfigSync(
+        path.resolve(__dirname, '../../../markdownlint-config-ali/index.json'),
+      );
+    } catch (e) {
+      // markdownlint-config-ali 位于 f2elint 下面的 node_modules
+      lintConfig.config = markdownLint.readConfigSync(
+        path.resolve(__dirname, '../../node_modules/markdownlint-config-ali/index.json'),
+      );
+    }
   } else {
     lintConfig.config = markdownLint.readConfigSync(path.resolve(cwd, lintConfigFiles[0]));
   }
