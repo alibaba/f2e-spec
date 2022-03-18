bfc7b1851978d0cde2fcb43b31197b13ec20486a
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-03-18 10:00:36 +0800

    fix: eslint test

--- packages/eslint-config-ali/test/use-babel-eslint.test.js
+++ packages/eslint-config-ali/test/use-babel-eslint.test.js
@@ -3,38 +3,45 @@ const eslint = require('eslint');
 const path = require('path');
 
 describe('test/use-babel-eslint.test.js', () => {
-  it('babel-eslint parser run well for react', () => {
+  it('babel-eslint parser run well for react', async () => {
     const configPath = './react.js';
     const filePath = path.join(__dirname, './fixtures/use-babel-eslint.jsx');
 
-    const cli = new eslint.CLIEngine({
-      configFile: configPath,
+    const cli = new eslint.ESLint({
+      overrideConfigFile: configPath,
       useEslintrc: false,
       ignore: false,
     });
 
-    const reports = cli.executeOnFiles([filePath]);
-    const results = reports.results[0];
-    const errorReportedByReactPlugin = results.messages.filter(result => {
+    const results = await cli.lintFiles([filePath]);
+    const { messages, errorCount, fatalErrorCount, warningCount } = results[0];
+
+    assert.equal(fatalErrorCount, 0);
+    assert.equal(errorCount, 27);
+    assert.equal(warningCount, 7);
+
+    const errorReportedByReactPlugin = messages.filter((result) => {
       return result.ruleId && result.ruleId.indexOf('react/') !== -1;
     });
 
     assert.notEqual(errorReportedByReactPlugin.length, 0);
   });
 
-  it('babel-eslint parser run well for vue', () => {
+  it('babel-eslint parser run well for vue', async () => {
     const configPath = './vue.js';
     const filePath = path.join(__dirname, './fixtures/vue.vue');
 
-    const cli = new eslint.CLIEngine({
-      configFile: configPath,
+    const cli = new eslint.ESLint({
+      overrideConfigFile: configPath,
       useEslintrc: false,
       ignore: false,
     });
 
-    const reports = cli.executeOnFiles([filePath]);
-    const results = reports.results[0];
+    const results = await cli.lintFiles([filePath]);
+    const { errorCount, fatalErrorCount, warningCount } = results[0];
 
-    assert.ok(results.messages.length > 1);
+    assert.equal(fatalErrorCount, 0);
+    assert.equal(errorCount, 4);
+    assert.equal(warningCount, 0);
   });
 });
