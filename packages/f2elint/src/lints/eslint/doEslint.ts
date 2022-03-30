52bb3ab4883a17e531c813815654bf9285093b36
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-03-30 14:13:14 +0800

    fix: ignore pattern

--- packages/f2elint/src/lints/eslint/doEslint.ts
+++ packages/f2elint/src/lints/eslint/doEslint.ts
@@ -1,7 +1,7 @@
 import { Config, PKG, ScanOptions } from 'types';
 import { ESLINT_FILE_EXT, ESLINT_IGNORE_PATTERN } from 'utils/constants';
 import fg from 'fast-glob';
-import { extname } from 'path';
+import { extname, join } from 'path';
 import { ESLint } from 'eslint';
 import { getESLintConfig } from './getESLintConfig';
 import { formatESLintResults } from './formatESLintResults';
@@ -12,11 +12,14 @@ export interface DoESLintOptions extends ScanOptions {
 }
 
 export async function doESLint(options: DoESLintOptions) {
-  let files: string[] = [];
+  let files: string[];
   if (options.files) {
     files = options.files.filter((name) => ESLINT_FILE_EXT.includes(extname(name)));
   } else {
-    const pattern = `**/*.{${ESLINT_FILE_EXT.map((t) => t.replace(/^\./, '')).join(',')}}`;
+    const pattern = join(
+      options.include,
+      `**/*.{${ESLINT_FILE_EXT.map((t) => t.replace(/^\./, '')).join(',')}}`,
+    );
     files = await fg(pattern, {
       cwd: options.cwd,
       ignore: ESLINT_IGNORE_PATTERN,
