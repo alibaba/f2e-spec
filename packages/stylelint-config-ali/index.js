9fe3261630fa66fca6596be20838f77334e665f7
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2023-10-11 11:45:12 +0800

    feat(stylelint-config-ali): remove 13 deprecated stylelint rules

--- packages/stylelint-config-ali/index.js
+++ packages/stylelint-config-ali/index.js
@@ -23,7 +23,6 @@ module.exports = {
     'no-duplicate-at-import-rules': true,
     'no-duplicate-selectors': true,
     'no-empty-source': null,
-    'no-extra-semicolons': true,
     'no-invalid-double-slash-comments': true,
     'property-no-unknown': true,
     'selector-pseudo-class-no-unknown': [
@@ -52,33 +51,16 @@ module.exports = {
      * Stylistic issues
      * @link https://stylelint.io/user-guide/rules/list#stylistic-issues
      */
-    indentation: 2,
-    'block-closing-brace-newline-before': 'always-multi-line',
-    'block-closing-brace-space-before': 'always-single-line',
-    'block-opening-brace-newline-after': 'always-multi-line',
-    'block-opening-brace-space-before': 'always',
-    'block-opening-brace-space-after': 'always-single-line',
-    'color-hex-case': 'lower',
     'color-hex-length': 'short',
     'comment-whitespace-inside': 'always',
-    'declaration-colon-space-before': 'never',
-    'declaration-colon-space-after': 'always',
     'declaration-block-single-line-max-declarations': 1,
-    'declaration-block-trailing-semicolon': [
-      'always',
-      {
-        severity: 'error',
-      },
-    ],
     'length-zero-no-unit': [
       true,
       {
         ignore: ['custom-properties'],
       },
     ],
-    'max-line-length': 100,
     'selector-max-id': 0,
-    'value-list-comma-space-after': 'always-single-line',
   },
   overrides: [
     // scss language support
