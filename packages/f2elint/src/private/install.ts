35e4ab9ec394bcd115a404bdd3d7f79e1d686e5b
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2024-02-06 17:46:30 +0800

    fix install

--- packages/f2elint/src/private/install.ts
+++ packages/f2elint/src/private/install.ts
@@ -1,8 +1,18 @@
 import { spawn } from 'child_process';
+import commandExists from 'command-exists';
 
-export function install(projectPath: string) {
-  return new Promise<void>((res, rej) => {
-    const child = spawn(`${process.env.npm_command} update -f`, {
+export async function install(projectPath: string) {
+  let npm_command = 'npm';
+  if (await commandExists('tnpm')) {
+    // tnpm of Alibaba and Ant Group
+    npm_command = 'tnpm';
+  } else if (process.env.npm_command) {
+    // pnpm, yarn support npm_command environment variable
+    npm_command = process.env.npm_command;
+  }
+
+  await new Promise<void>((res, rej) => {
+    const child = spawn(`${npm_command} update -f`, {
       cwd: projectPath,
       shell: true,
     });
