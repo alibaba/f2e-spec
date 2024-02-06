fd74c68258000b5f6a33563ea579e96767ea17da
Guo Yunhe <i@guoyunhe.me>
2024-02-06 19:08:46 +0800

    support cnpm

--- packages/f2elint/src/private/install.ts
+++ packages/f2elint/src/private/install.ts
@@ -3,12 +3,15 @@ import commandExists from 'command-exists';
 
 export async function install(projectPath: string) {
   let npm_command = 'npm';
-  if (await commandExists('tnpm')) {
-    // tnpm of Alibaba and Ant Group
-    npm_command = 'tnpm';
-  } else if (process.env.npm_command) {
+  if (process.env.npm_command) {
     // pnpm, yarn support npm_command environment variable
     npm_command = process.env.npm_command;
+  } else if (await commandExists('tnpm')) {
+    // tnpm of Alibaba and Ant Group
+    npm_command = 'tnpm';
+  } else if (await commandExists('cnpm')) {
+    // cnpm of China
+    npm_command = 'cnpm';
   }
 
   await new Promise<void>((res, rej) => {
