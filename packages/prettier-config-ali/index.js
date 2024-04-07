b573cc9d69a8aee99a62ebd3af0307754bf3f973
Guo Yunhe <i@guoyunhe.me>
2024-04-07 22:42:18 +0800

    feat(prettier): change to esm package

--- packages/prettier-config-ali/index.js
+++ packages/prettier-config-ali/index.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   // 详见 https://prettier.io/docs/en/options
   printWidth: 100,
   tabWidth: 2,
@@ -14,7 +14,9 @@ module.exports = {
   endOfLine: 'lf',
   // 从 Prettier 3.x 开始，插件必须配置才能生效
   plugins: [
-    // 排序 package.json 中的属性，ESM 项目报错，暂时禁用
-    'prettier-plugin-packagejson',
+    // 排序 package.json 中的属性
+    import.meta.resolve('prettier-plugin-packagejson'),
   ],
 };
+
+export default config;
