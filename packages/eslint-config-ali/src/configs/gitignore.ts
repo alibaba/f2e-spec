9375b2b6a95d89859807dd9e8a16caa20bf67686
Guo Yunhe <i@guoyunhe.me>
2025-02-26 21:11:18 +0800

    fix(eslint-config-ali): .gitignore not exist

--- packages/eslint-config-ali/src/configs/gitignore.ts
+++ packages/eslint-config-ali/src/configs/gitignore.ts
@@ -1,8 +1,10 @@
 import { includeIgnoreFile } from '@eslint/compat';
 import { Linter } from 'eslint';
+import { existsSync } from 'node:fs';
 import path from 'node:path';
 
 /** Include ignore patterns from `.gitignore` at project root */
-const gitignore: Linter.Config = includeIgnoreFile(path.join(process.cwd(), '.gitignore'));
+const gitignorePath = path.join(process.cwd(), '.gitignore');
+const gitignore: Linter.Config = existsSync(gitignorePath) ? includeIgnoreFile(gitignorePath) : {};
 
 export default gitignore;
