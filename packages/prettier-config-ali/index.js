2b5570cee0146b23a5ca9c3449a799b29841d0d6
Guo Yunhe <i@guoyunhe.me>
2025-04-27 20:26:33 +0800

    fix(prettier-config-ali): plugin

--- packages/prettier-config-ali/index.js
+++ packages/prettier-config-ali/index.js
@@ -1,3 +1,9 @@
+import packagejson from 'prettier-plugin-packagejson';
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
 const config = {
   // 详见 https://prettier.io/docs/en/options
   printWidth: 100,
@@ -14,9 +20,8 @@ const config = {
   endOfLine: 'lf',
   // 从 Prettier 3.x 开始，插件必须配置才能生效
   plugins: [
-    // VS Code 的 Prettier 插件目前有问题，不支持 import.meta.resolve
     // 排序 package.json 中的属性
-    // import.meta.resolve('prettier-plugin-packagejson'),
+    packagejson,
   ],
 };
 
