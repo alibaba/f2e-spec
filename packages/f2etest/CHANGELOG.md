f2772799712fd57063b8151cc2e4a0242c5e8296
Guo Yunhe <i@guoyunhe.me>
2025-04-27 20:34:39 +0800

    chore(f2etest): v1.0.0

---
+++ packages/f2etest/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.0 - 2025-04-27
+
+- Added `base`, `web`, `react` and `preact` template
---
+++ packages/f2etest/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { base } from 'eslint-config-ali';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default [...base, prettier];
